a60888d2e18e957e4e961a3bdc3dceea5b3a3067
HiroRin753 <chikuzen.0306@gmail.com>
2021-03-24 23:39:05 +0900

    未ログインユーザーリダイレクト

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -38,5 +38,11 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 
 end
