5ccd4a9d0f223ff9d9ff28690227735d1bfb58c7
HiroRin753 <chikuzen.0306@gmail.com>
2021-04-07 16:29:21 +0900

    emailテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe User, type: :model do
       expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
-
+      user = User.new(nickname: 'test', email:'', password: '000000', password_confirmation: '000000')
     end
   end
  
